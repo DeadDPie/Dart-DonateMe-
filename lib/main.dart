181edaff941615d58aa8b79c3cee7c5a11a866db
SahnovAlex <sahnov2018@inbox.ru>
2023-03-20 00:14:19 +0700

    Полная реструктуризация ресурсов приложения.
    Вёрстка окон: "Авторизация", "Выбор роли", "Регистрация".

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:donate_me/screens/AuthorizationForm.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainForm());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp ({super.key});
+class MainForm extends StatelessWidget {
+  const MainForm ({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Главное_окно',
-      home: Scaffold(
-        backgroundColor: Color.fromRGBO(241, 224, 203, 1.0),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(1.0),
-                  child: Text(
-                    'ДОБРО \nПОЖАЛОВАТЬ',
-                    style: TextStyle(fontFamily: 'SourceSans', height: 1,
-                      color: Color.fromRGBO(106, 62, 112, 1.0),
-                        fontSize: 42.0, fontWeight: FontWeight.w800),
-                        textAlign: TextAlign.center),
-                ),
-                const Text(
-                  'ПРЕЖДЕ ЧЕМ МЫ НАЧНЁМ...',
-                  style: TextStyle(
-                    color: Color.fromRGBO(140, 104, 144, 1.0),
-                    fontSize: 14.5, fontWeight: FontWeight.w400,
-                    fontStyle: FontStyle.italic)),
-
-                Padding(
-                  padding: EdgeInsets.fromLTRB(40, 15, 40, 5),
-                  child: TextField(decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        color: Colors.white,
-                        width: 5
-                      )),
-                    hintText: 'ВВЕДИТЕ НИК',
-                    hintStyle: const TextStyle(fontSize: 14.5,
-                    fontWeight: FontWeight.normal, fontStyle: FontStyle.italic),
-                    fillColor: Color.fromRGBO(255, 255, 255, 1.0),
-                    filled: true
-                  ))
-                ),
-
-                Padding(
-                  padding: EdgeInsets.fromLTRB(40, 15, 40, 5),
-                  child: TextField(decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(
-                          color: Colors.white,
-                          width: 5
-                        )),
-                      hintText: 'ВВЕДИТЕ ПАРОЛЬ',
-                      hintStyle: const TextStyle(fontSize: 14.5,
-                      fontWeight: FontWeight.normal, fontStyle: FontStyle.italic),
-                      fillColor: Color.fromRGBO(255, 255, 255, 1.0),
-                      filled: true
-                  ),
-                      obscureText: true
-                  )
-                ),
-
-
-                TextButton(
-                    onPressed: _gotoRegisration,
-                    child: const Text('У МЕНЯ НЕТ АККАУНТА',
-                    style: TextStyle(
-                      color: Color.fromRGBO(146, 106, 86, 1.0),
-                      fontSize: 14.5, fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.italic,
-                      decoration: TextDecoration.underline))
-                ),
-                
-                Padding(
-                    padding: EdgeInsets.fromLTRB(100, 5, 100, 5),
-                  child: OutlinedButton(
-                    onPressed: _button1,
-                    style: OutlinedButton.styleFrom(
-
-                      backgroundColor: Color.fromRGBO(156, 194, 186, 1.0),
-                      side: const BorderSide(width: 3.0, color: Colors.white),
-                      minimumSize: const Size(240, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                    )),
-                    child: const Text('ПРОДОЛЖИТЬ',
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 1.0),
-                      fontSize: 20.0, fontWeight: FontWeight.w800,
-                      fontStyle: FontStyle.normal
-                    ))),
-                ),
-              ],
-            ),
-          ),
-      ),
+      home: const AuthorizationForm(),
     );
   }
-
-  void _gotoRegisration() {}
-
-  void _button1() {}
 }
---
+++ lib/screens/RegistrationForm.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class RegistrationForm extends StatelessWidget{
+  const RegistrationForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Окно_Регистрации',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(200, 154, 132, 1.0),
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back, 
+                color: Color.fromRGBO(116, 56, 113, 1.0)
+              ),
+          onPressed:() => Navigator.pop(context, false)),
+          centerTitle: true,
+          title: const Text('ПРИВЕТСТВУЕМ!',
+          style: TextStyle(
+            color: Color.fromRGBO(141, 80, 58, 1.0),
+            fontSize: 25.0, fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.normal
+          ),
+          ),
+       ),
+        backgroundColor: Color.fromRGBO(241, 227, 211, 1.0),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 70, 25, 9),
+                child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                      width: 5)),
+                  fillColor: Color.fromRGBO(255, 255, 255, 1.0),
+                  filled: true,
+                  labelText: 'КАК К ВАМ ОБРАЩАТЬСЯ?'
+                ))
+              ),
+              
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 20, 25, 9),
+                child:TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                          color: Colors.white,
+                          width: 5)),
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: 'ВЫБЕРЕТЕ СЕБЕ НИК'
+                ))
+              ),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 20, 25, 9),
+                child:TextField(decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.white,
+                            width: 5)),
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: 'ПРИДУМАЙТЕ ПАРОЛЬ',
+                  hintText: '(НЕ МЕНЕЕ 8 СИМВОЛОВ И ХОТЯ БЫ ОДНА ЦИФРА)',
+                  hintStyle: const TextStyle(fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.italic
+                  )
+                ))
+              ),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
+                child: OutlinedButton(
+                  onPressed: (){},
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(134, 194, 186, 1.0),
+                    side: const BorderSide(width: 3.0, color: Colors.white),
+                    minimumSize: Size(300, 80),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    )),
+                  child: const Text('ПРОДОЛЖИТЬ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0, fontWeight: FontWeight.w800,
+                      fontStyle: FontStyle.normal
+                    )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
